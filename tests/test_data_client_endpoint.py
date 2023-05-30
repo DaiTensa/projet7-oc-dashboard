598e072513f2720903b8a5041d747fdc14f18c06
DaiTensa <dai.tensaout@gmail.com>
2023-05-30 13:20:19 +0200

    test data client

--- tests/test_data_client_endpoint.py
+++ tests/test_data_client_endpoint.py
@@ -6,17 +6,18 @@ clients_data__path = "https://raw.githubusercontent.com/DaiTensa/dashboard/main/
 data_clients = pd.read_csv(clients_data__path)
 
 def get_data_as_df(df_clients, ID_client):
-    ID_client = int(ID_client)
-    df_client= (df_clients.loc[df_clients["SK_ID_CURR"] == ID_client]).copy()
+    id = int(ID_client)
+    df_client= (df_clients.loc[df_clients["SK_ID_CURR"] == id]).copy()
     return df_client
 
 
-ID_CLIENT = 100038
 
-df_client = get_data_as_df(data_clients, ID_client= ID_CLIENT)
+
+
 
 @pytest.mark.infos_clients
-def test_infos_du_client(df_client):
+def test_infos_du_client():
+    df_client = get_data_as_df(data_clients, ID_client= 100038)
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'CODE_GENDER'] == 1
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'AMT_INCOME_TOTAL'] == 180000
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'AMT_CREDIT'] == 625500
