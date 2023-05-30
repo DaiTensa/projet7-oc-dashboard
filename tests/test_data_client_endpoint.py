e0b930dd6f70ed74c19427862439961c0b96c5b2
DaiTensa <dai.tensaout@gmail.com>
2023-05-30 12:12:00 +0200

    test data client

---
+++ tests/test_data_client_endpoint.py
@@ -0,0 +1,14 @@
+import pytest
+from src.pipeline.predict_pipeline import DataClient
+
+
+data_clients = DataClient()
+ID_CLIENT = 100038
+
+df_client = data_clients.get_data_as_df(ID_client= int(ID_CLIENT))
+
+@pytest.mark.infos_clients
+def test_infos_du_client():
+    assert df_client['CODE_GENDER'] == 1
+    assert df_client['AMT_INCOME_TOTAL'] == 180000
+    assert df_client['AMT_CREDIT'] == 625500
