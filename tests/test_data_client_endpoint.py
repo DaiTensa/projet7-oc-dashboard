067c0903e94866acf65f369f086ecd0c3c705c7c
DaiTensa <dai.tensaout@gmail.com>
2023-05-30 12:58:53 +0200

    test data client

--- tests/test_data_client_endpoint.py
+++ tests/test_data_client_endpoint.py
@@ -1,11 +1,20 @@
 import pytest
-from predict_pipeline import DataClient
+import pandas as pd
+
+clients_data__path = "https://raw.githubusercontent.com/DaiTensa/dashboard/main/reduced_test.csv"
+
+data_clients = pd.read_csv(clients_data__path)
+
+def get_data_as_df(df_clients, ID_client):
+    ID_client = int(ID_client)
+    df_client= (df_clients.loc[df_clients["SK_ID_CURR"] == ID_client]).copy()
+    df_client = df_client.drop(["SK_ID_CURR"], axis=1)
+    return df_client
 
 
-data_clients = DataClient()
 ID_CLIENT = 100038
 
-df_client = data_clients.get_data_as_df(ID_client= int(ID_CLIENT))
+df_client = get_data_as_df(data_clients, ID_client= ID_CLIENT)
 
 @pytest.mark.infos_clients
 def test_infos_du_client():
