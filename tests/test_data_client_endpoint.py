74163f332f1dcf7d9d9d5a5ea9d85ef9f3f74271
DaiTensa <dai.tensaout@gmail.com>
2023-05-30 13:12:08 +0200

    test data client

--- tests/test_data_client_endpoint.py
+++ tests/test_data_client_endpoint.py
@@ -16,7 +16,7 @@ ID_CLIENT = 100038
 df_client = get_data_as_df(data_clients, ID_client= ID_CLIENT)
 
 @pytest.mark.infos_clients
-def test_infos_du_client():
+def test_infos_du_client(df_client):
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'CODE_GENDER'] == 1
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'AMT_INCOME_TOTAL'] == 180000
     assert df_client.loc[df_client["SK_ID_CURR"] == 100038 , 'AMT_CREDIT'] == 625500
